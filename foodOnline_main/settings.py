89ce5a9f7575fa02e6175623447008d6f0c96651
parzivald <mukhammadkodirmakhmudjanov@gmail.com>
2023-01-20 18:24:19 +0500

    Token verification & Email configuration

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -144,3 +144,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# EMAIL CONFIG
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <onlinefood.django@gmail.com>'
